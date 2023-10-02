4c562e405c48fa4d981c1c24b17515bea126a855
Dev <devesh1245sharma@gmail.com>
2023-10-02 12:36:50 +0530

    first commit

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -6,15 +6,23 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyApAHHVSD-9BId-GMj7BAA0IUX91LeosB0",
-  authDomain: "expense-tracker-d14e8.firebaseapp.com",
-  projectId: "expense-tracker-d14e8",
-  storageBucket: "expense-tracker-d14e8.appspot.com",
-  messagingSenderId: "738196435189",
-  appId: "1:738196435189:web:007916e2e57f751fe6d5bc",
+  apiKey: "AIzaSyDvP1-CfBZ1crZ5tlJgHUdi291Yo-qKL4c",
+  authDomain: "expanse-tracker-9add1.firebaseapp.com",
+  projectId: "expanse-tracker-9add1",
+  storageBucket: "expanse-tracker-9add1.appspot.com",
+  messagingSenderId: "832509692458",
+  appId: "1:832509692458:web:5f5137d73930596f9d1153"
 };
 
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
